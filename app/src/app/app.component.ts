4c4a1d1dcee6c55ebcce770df91382a716605d0d
K. Largaton <105814979+Largaton1@users.noreply.github.com>
2025-04-07 21:24:54 +0200

    fix: app

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  standalone: false,
+  standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
