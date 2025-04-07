e7d0ae8f6ece3c59358c716638ba8b609b0a54dc
K. Largaton <105814979+Largaton1@users.noreply.github.com>
2025-04-08 01:26:38 +0200

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { EvenementDetailsComponent } from './modules/evenement/evenement-details/evenement-details.component';
+import { EvenementListComponent } from './modules/evenement/evenement-list/evenement-list.component';
+import { TicketCheckoutComponent } from './modules/ticket/ticket-checkout/ticket-checkout.component';
+import { UtilisateurTicketsComponent } from './modules/ticket/utilisateur-tickets/utilisateur-tickets.component';
+import { LoginComponent } from './modules/utilisateur/login/login.component';
+import { RegisterComponent } from './modules/utilisateur/register/register.component';
+
+const routes: Routes = [
+  { path: '', component: EvenementListComponent },
+  { path: 'evenement/:id', component: EvenementDetailsComponent },
+  { path: 'evenement/:id/buy', component: TicketCheckoutComponent },
+  { path: 'utilisateur/tickets', component: UtilisateurTicketsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
