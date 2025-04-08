a8fc33eb8696dc3c62d2eb396382df8173ad15de
K. Largaton <105814979+Largaton1@users.noreply.github.com>
2025-04-08 14:02:37 +0200

    Integration template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { EvenementDetailsComponent } from './modules/evenement/evenement-details/evenement-details.component';
 import { EvenementListComponent } from './modules/evenement/evenement-list/evenement-list.component';
+import { FooterComponent } from './modules/shared/footer/footer.component';
+import { NavbarComponent } from './modules/shared/navbar/navbar.component';
+import { SidebarComponent } from './modules/shared/sidebar/sidebar.component';
 import { TicketCheckoutComponent } from './modules/ticket/ticket-checkout/ticket-checkout.component';
 import { UtilisateurTicketsComponent } from './modules/ticket/utilisateur-tickets/utilisateur-tickets.component';
 import { LoginComponent } from './modules/utilisateur/login/login.component';
 import { RegisterComponent } from './modules/utilisateur/register/register.component';
 
 const routes: Routes = [
+  { path: 'sidebar', component: SidebarComponent },
+  { path: 'navbar', component: NavbarComponent },
+  { path: 'footer', component: FooterComponent },
   { path: '', component: EvenementListComponent },
   { path: 'evenement/:id', component: EvenementDetailsComponent },
   { path: 'evenement/:id/buy', component: TicketCheckoutComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './modules/shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent
+
+  
   ],
   imports: [
     BrowserModule,
